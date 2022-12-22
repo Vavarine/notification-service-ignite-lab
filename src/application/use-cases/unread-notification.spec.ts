adafe02a86513a646a08229a0d5ca72182f44da4
vavarine <evailson.m.barbosa@gmail.com>
2022-12-22 21:16:22 +0500

    feat: read & unread notification

---
+++ src/application/use-cases/unread-notification.spec.ts
@@ -0,0 +1,29 @@
+import { Notification } from '@application/entities/notification';
+import { makeNotification } from '@test/factories/notification-factory';
+import { InMemoryNotificationsRepository } from '../../../test/repositories/in-memory-notifications-repository';
+import { NotificationNotFound } from './errors/notification-not-found';
+import { ReadNotification } from './read-notification';
+import { UnreadNotification } from './unread-notification';
+
+describe('Unread notification', () => {
+  it('should be able to unread a notification', async () => {
+    const notificationsRepository = new InMemoryNotificationsRepository();
+    const unreadNotification = new UnreadNotification(notificationsRepository);
+
+    const notification = new Notification(makeNotification());
+
+    await notificationsRepository.create(notification);
+    await unreadNotification.execute({ notificationId: notification.id });
+
+    expect(notificationsRepository.notifications[0].readAt).toBeNull();
+  });
+
+  it('should not be able to unread a non existing notification', async () => {
+    const notificationsRepository = new InMemoryNotificationsRepository();
+    const readNotification = new ReadNotification(notificationsRepository);
+
+    await expect(
+      readNotification.execute({ notificationId: '1' }),
+    ).rejects.toThrow(NotificationNotFound);
+  });
+});
